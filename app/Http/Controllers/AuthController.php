d6e30e60209536a542a06ae64bfae6bf3cfe606c
jorginho222 <ivan.abel22@gmail.com>
2023-01-31 23:03:22 -0300

    Sanctum login auth. Implementation in area crud

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+           'email' => 'email|required',
+           'password' => 'required',
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+               'message' => 'Credenciales inválidas',
+            ], 422);
+        }
+
+        $user = User::query()->where('email', $request->email)->first();
+
+        $authToken = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+           'access_token' => $authToken,
+        ]);
+    }
+}
